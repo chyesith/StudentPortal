ed172593102ceb073f09468a85b4bc32e5c551e9
Yesith <108515089+yesith94@users.noreply.github.com>
2023-03-11 20:10:52 +0000

    added repositories and entities for course class

--- src/main/java/com/example/studentportal/models/Course.java
+++ src/main/java/com/example/studentportal/models/Course.java
@@ -1,9 +1,30 @@
 package com.example.studentportal.models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+
+@Entity(name = "Course")
 public class Course {
+    @Id
+    @SequenceGenerator(name = "course_sequence",
+            sequenceName = "course_sequence",
+            allocationSize = 1
+
+    )
+    @Column(name = "course_id", updatable = false)
     Long courseId;
+
+    @Column(name = "description",  nullable = false , columnDefinition = "TEXT")
+
     String description;
+    @Column(name = "course_name", nullable = false , columnDefinition = "TEXT")
+
     String courseName;
+
+    @Column(name = "course_fee",  nullable = false)
+    double courseFee;
 
     public Course(Long courseId, String description, String courseName) {
         this.courseId = courseId;
