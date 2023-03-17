a18b747e39a78c4d343cd5056e720e15417d3786
Yesith <108515089+yesith94@users.noreply.github.com>
2023-03-17 00:26:45 +0000

    security page disabled

--- src/main/java/com/example/studentportal/StudentPortalApplication.java
+++ src/main/java/com/example/studentportal/StudentPortalApplication.java
@@ -2,8 +2,9 @@ package com.example.studentportal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class StudentPortalApplication {
 
     public static void main(String[] args) {
