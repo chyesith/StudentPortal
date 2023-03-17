d7b6250b14c5f5a05d6d6c951447d376ae58e134
Yesith <108515089+yesith94@users.noreply.github.com>
2023-03-17 23:01:31 +0000

    addded changes for course

--- src/main/java/com/example/studentportal/models/Course.java
+++ src/main/java/com/example/studentportal/models/Course.java
@@ -1,9 +1,13 @@
 package com.example.studentportal.models;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
 
 @Entity(name = "Course")
+@AllArgsConstructor
+@NoArgsConstructor
 public class Course {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
